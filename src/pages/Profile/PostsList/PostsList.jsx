d97a7effc2fc11656f7b4f73f2ced9cf276b2a1b
RusKom27 <48696676+RusKom27@users.noreply.github.com>
2022-11-25 18:48:23 +0200

    update

--- src/pages/Profile/PostsList/PostsList.jsx
+++ src/pages/Profile/PostsList/PostsList.jsx
@@ -1,15 +1,20 @@
 import React from "react"
 import styles from "./PostsList.module.scss"
 import {Post} from "./Post/Post";
+import axios from "axios";
 
 
 function PostsList({posts}) {
-
     posts = posts.map((post, i) => <Post key={i} post={post}/>)
+
+    const test_axios = () => {
+        axios.get("https://nodejs-postgresql-lyart.vercel.app").then(resp => console.log(resp.data))
+    }
 
     return (
         <div>
             <h2>Posts</h2>
+            <button onClick={test_axios}>Test</button>
             <hr/>
             <div className={styles.posts_list}>
                 {posts}
