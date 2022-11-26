9ae3419ebd00adc9910ad848b7adf2071c38dc28
RusKom27 <48696676+RusKom27@users.noreply.github.com>
2022-11-26 09:44:01 +0200

    update

--- src/pages/Profile/PostsList/PostsList.jsx
+++ src/pages/Profile/PostsList/PostsList.jsx
@@ -7,14 +7,18 @@ import axios from "axios";
 function PostsList({posts}) {
     posts = posts.map((post, i) => <Post key={i} post={post}/>)
 
-    const test_axios = () => {
-        axios.get("https://nodejs-postgresql-lyart.vercel.app").then(resp => console.log(resp.data))
+    const test_axios_local = () => {
+        axios.get("http://localhost:3000").then(resp => console.log(resp.data))
+    }
+    const test_axios_deploy = () => {
+        axios.get("https://nodejs-postgresql-ruskom27.vercel.app").then(resp => console.log(resp.data))
     }
 
     return (
         <div>
             <h2>Posts</h2>
-            <button onClick={test_axios}>Test</button>
+            <button onClick={test_axios_local}>Test on local</button>
+            <button onClick={test_axios_deploy}>Test on deploy</button>
             <hr/>
             <div className={styles.posts_list}>
                 {posts}
