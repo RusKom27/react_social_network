b6687f6714237af6cdb8ca7daa5a3e30f0b0ca7c
RusKom27 <48696676+RusKom27@users.noreply.github.com>
2022-11-26 19:04:02 +0200

    update

--- src/pages/Profile/PostsList/PostsList.jsx
+++ src/pages/Profile/PostsList/PostsList.jsx
@@ -7,18 +7,38 @@ import axios from "axios";
 function PostsList({posts}) {
     posts = posts.map((post, i) => <Post key={i} post={post}/>)
 
-    const test_axios_local = () => {
+    const get_axios_local = () => {
         axios.get("http://localhost:3000").then(resp => console.log(resp.data))
     }
-    const test_axios_deploy = () => {
-        axios.get("https://nodejs-pg-ruskom27.vercel.app").then(resp => console.log(resp.data))
+    const post_axios_local = () => {
+        axios.post(
+            "http://localhost:3000/quotes",
+            {
+                quote: "tessdfswedwt" + Date.now(),
+                author: "teeferdwest" + Date.now(),
+            })
+        .then(data => console.log(data.data))
+    }
+    const get_axios_deploy = () => {
+        axios.get("https://nodejs-pg.vercel.app/quotes?page=8").then(resp => console.log(resp.data))
+    }
+    const post_axios_deploy = () => {
+        axios.post(
+            "https://nodejs-pg.vercel.app/quotes",
+            {
+                quote: "tessdfswedwt" + Date.now(),
+                author: "teeferdwest" + Date.now()
+            })
+            .then(data => console.log(data.data))
     }
 
     return (
         <div>
             <h2>Posts</h2>
-            <button onClick={test_axios_local}>Test on local</button>
-            <button onClick={test_axios_deploy}>Test on deploy</button>
+            <button onClick={get_axios_local}>Get on local</button>
+            <button onClick={post_axios_local}>Post on local</button>
+            <button onClick={get_axios_deploy}>Get on deploy</button>
+            <button onClick={post_axios_deploy}>Post on deploy</button>
             <hr/>
             <div className={styles.posts_list}>
                 {posts}
