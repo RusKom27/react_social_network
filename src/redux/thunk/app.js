902537b7e92ecc3c138c5760d8ad5680ca5562a8
RusKom27 <48696676+RusKom27@users.noreply.github.com>
2023-01-14 10:36:00 +0200

    refactoring ably channels

--- src/redux/thunk/app.js
+++ src/redux/thunk/app.js
@@ -5,24 +5,35 @@ import {subscribeToFeedPostsChannel, subscribeToMessageChannel} from "./socket_s
 import {getMessages} from "./messages";
 import {initialize} from "../reducers/app";
 import {subscribeToChannel} from "../../packages/ably";
-import {updatePost} from "../reducers/feed";
+import {addPost, updatePost} from "../reducers/feed";
+import {addDialog, addMessage, updateMessage} from "../reducers/messages";
 
 export const initializeApp = (token) => async (dispatch) => {
     config.token = token
     return await UserAPI.getUser().then(user => {
         subscribeToChannel(user.data._id, (message) => {
-            console.log(message)
             switch (message.name) {
                 case 'post_like':
-                    console.log("like")
                     dispatch(updatePost(message.data))
+                    break
+                case 'new_post':
+                    dispatch(addPost(message.data))
+                    break
+                case 'new_message':
+                    dispatch(addMessage(message.data))
+                    break
+                case 'new_dialog':
+                    dispatch(addDialog(message.data))
+                    break
+                case 'check_message':
+                    dispatch(updateMessage(message.data))
                     break
             }
         })
         dispatch(loginUser(user.data))
         dispatch(getMessages())
-        subscribeToMessageChannel(dispatch)
-        subscribeToFeedPostsChannel(dispatch, user.data.login)
+        // subscribeToMessageChannel(dispatch)
+        // subscribeToFeedPostsChannel(dispatch, user.data.login)
         dispatch(initialize())
     })
 }
--- src/redux/thunk/socket_subscriptions/feed_posts.js
+++ src/redux/thunk/socket_subscriptions/feed_posts.js
@@ -2,20 +2,14 @@ import {CHANNEL, subscribeToChannel} from "../../../packages/ably";
 import {addPost, deletePost, updatePost} from "../../reducers/feed";
 
 export const subscribeToFeedPostsChannel = (dispatch, user_login) => {
-    // subscribeToChannel(CHANNEL.POSTS, message => {
-    //     if (!(user_login === "" || message.data.user.login === user_login)) return
-    //     switch (message.name) {
-    //         case 'new_post':
-    //             dispatch(addPost(message.data))
-    //             break
-    //         case 'delete_post':
-    //             dispatch(deletePost(message.data))
-    //             break
-    //         case 'check_post':
-    //             dispatch(updatePost(message.data))
-    //             break
-    //         default:
-    //             console.log(message)
-    //     }
-    // })
+    subscribeToChannel(CHANNEL.POSTS, message => {
+        if (!(user_login === "" || message.data.user.login === user_login)) return
+        switch (message.name) {
+            case 'new_post':
+                dispatch(addPost(message.data))
+                break
+            default:
+                console.log(message)
+        }
+    })
 }
